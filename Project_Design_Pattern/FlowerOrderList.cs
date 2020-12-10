9b3ea41c9ca0705522e51616ca3148fc74f0e392
Sakura <golsa_n@outlook.com>
2020-12-10 14:57:30 +0100

    Comment Deleted on FlowerOrderList File

--- Project_Design_Pattern/FlowerOrderList.cs
+++ Project_Design_Pattern/FlowerOrderList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -37,7 +37,7 @@ namespace Project_Design_Pattern
                     
 
             }
-           //  Console.WriteLine(" **************** ");
+           
         }
     }
 }
